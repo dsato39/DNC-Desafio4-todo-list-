7c9df784615b48a7ed07ebaac220765854747e38
dsato39 <dsato39@gmail.com>
2023-11-26 13:12:42 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# DNC-Desafio3-todo-list
+Este é o projeto do Desafio 3 da formação em Tecnologia da Escola DNC. 
+
+link do site: https://dnc-desafio3-todolist-danielsato.netlify.app/
---
+++ src/assets/components/task_body/task_body.jsx
@@ -0,0 +1,30 @@
+// Para usar o if, utilizar operador ternário : e ?
+// Por exemplo {variável ? (valor para verdadeiro) : (valor para falso)}
+
+import './style.scss'
+import checked from '../../images/checked.svg'
+import unchecked from '../../images/unchecked.svg'
+import pencil from '../../images/pencil.svg'
+import trash from '../../images/trash.svg'
+import { Link } from 'react-router-dom'
+
+const Task_body = ({data}) => {
+  return (
+    <div className='task_body'>
+        <table>
+            <tbody>
+                <tr>
+                    <td colSpan="2">{data.title}</td>
+                    <td>{data.completed ? <img src={checked} alt="error"/> : <img src={unchecked} alt="error"/>}</td>
+                    <td>
+                      <Link to={`/edit/${data.id}`}><img src={pencil} alt="edit" /></Link>
+                      <Link to={`/delete/${data.id}`}><img src={trash} alt="delete" /></Link>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+  )
+}
+
+export default Task_body
